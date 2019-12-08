9ac1a14c4eede8e0905e153ace84a8fa1ee7cceb
ifmoon <ifmoon.io@gmail.com>
2019-12-08 18:07:21 +0900

    config 수정

--- config.js
+++ config.js
@@ -1,45 +1,39 @@
 const config = {
-	"gatsby": {
-		"pathPrefix": "/",
-		"siteUrl": "https://learn.hasura.io",
-		"gaTrackingId": null
-	},
-	"header": {
-		"logo": "https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/favicon.png",
-		"logoLink": "https://learn.hasura.io",
-		"title": "Gatsby Gitbook Boilerplate",
-		"githubUrl": "https://github.com/hasura/gatsby-gitbook-boilerplate",
-		"helpUrl": "",
-		"tweetText": "",
-		"links": [
-			{ "text": "", "link": ""}
-		],
-		"search": {
-			"enabled": false,
-			"indexName": "",
-			"algoliaAppId": process.env.GATSBY_ALGOLIA_APP_ID,
-			"algoliaSearchKey": process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-			"algoliaAdminKey": process.env.ALGOLIA_ADMIN_KEY
-		}
-	},
-	"sidebar": {
-		"forcedNavOrder": [
-			"/introduction",
-    		"/codeblock"
-		],
-		"links": [
-			{ "text": "Hasura", "link": "https://hasura.io"},
-		],
-		"frontline": false,
-		"ignoreIndex": true,
-	},
-	"siteMetadata": {
-		"title": "Gatsby Gitbook Boilerplate | Hasura",
-		"description": "Documentation built with mdx. Powering learn.hasura.io ",
-		"ogImage": null,
-		"docsLocation": "https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content",
-		"favicon": "https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg"
-	},
+  gatsby: {
+    pathPrefix: "/",
+    siteUrl: "https://j3d-team.github.io",
+    gaTrackingId: null
+  },
+  header: {
+    logo: "",
+    logoLink: "https://j3d-team.github.io",
+    title: "J3D Team Blog",
+    githubUrl: "https://github.com/j3d-team/j3d-team.github.io",
+    helpUrl: "",
+    tweetText: "",
+    links: [{ text: "", link: "" }],
+    search: {
+      enabled: false,
+      indexName: "",
+      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
+      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY
+    }
+  },
+  sidebar: {
+    forcedNavOrder: ["/introduction", "/codeblock"],
+    links: [],
+    frontline: false,
+    ignoreIndex: true
+  },
+  siteMetadata: {
+    title: "J3D 팀 블로그",
+    description: "입니다.",
+    ogImage: null,
+    docsLocation:
+      "https://github.com/j3d-team/j3d-team.github.io/tree/origin/content",
+    favicon: "https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg"
+  }
 };
 
 module.exports = config;
